cfedad753b82f6de673baeef3e62eaf8e2c6388e
Broc Jones <jon25044@byui.edu>
2026-04-22 12:26:56 -0600

    I did the prep2 activity for the day

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your percentage grade? (Use Whole Numbers) ");
+        string userPercentage = Console.ReadLine();
+        int percentage = int.Parse(userPercentage);
+        
+        string letter_grade = "0";
+
+        if (percentage >= 90)
+        {
+            letter_grade = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter_grade = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter_grade = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter_grade = "D";
+        }
+        else if (percentage >= 0)
+        {
+            letter_grade = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is {letter_grade}");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You Failed!");
+        }
+        
+
     }
 }
