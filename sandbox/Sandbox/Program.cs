c8dddb7e7ec6373dd2b96a7f94707a67d9621dc0
Broc Jones <jon25044@byui.edu>
2026-04-22 14:36:51 -0600

    I just added some stupid comments to the code

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
+        // I'm an amazing man
+        // and goodlooking too ;)
         Console.WriteLine("Hello Sandbox World!");
     }
 }
