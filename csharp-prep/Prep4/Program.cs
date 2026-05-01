eb4c61bda0b1e8826b11759f158af633218cdeb7
Broc Jones <jon25044@byui.edu>
2026-05-01 12:00:22 -0600

    I made the list program

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Numerics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int newNumber_to = -1;
+
+        do
+        {
+            Console.Write("Add a new number to the list (to stop enter 0)");
+            string newNumber = Console.ReadLine();
+            newNumber_to = int.Parse(newNumber);
+            if (newNumber_to != 0)
+            {
+                numbers.Add(newNumber_to);
+            }
+
+        } while(newNumber_to != 0);
+
+        int sum = 0;
+        int maxNum = 0;
+
+        foreach (int number in numbers)
+        {
+            sum += number;
+            if (number > maxNum)
+            {
+                maxNum = number;
+            }
+        }
+        Console.WriteLine($"The sum is {sum}");
+
+        double average = sum / numbers.Count;
+        Console.WriteLine($"The average is {average:0.00}");
+
+        Console.WriteLine($"The maximum number is {maxNum}");
+
     }
 }
