263487f79522e3daff00723edff2b87a73bc5722
Broc Jones <jon25044@byui.edu>
2026-05-01 11:31:16 -0600

    I made the number guessing game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Runtime.Versioning;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        int magicGuess = 0;
+        do
+        {
+            Console.Write("What is your guess? ");
+            string guess = Console.ReadLine();
+            magicGuess = int.Parse(guess);
+            if (magicGuess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (magicGuess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You got it right!");
+            }
+        } while (magicNumber != magicGuess);
     }
 }
