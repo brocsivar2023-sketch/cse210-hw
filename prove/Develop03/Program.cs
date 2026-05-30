acf635654a140e31dc0d43cd50da65715e9f5a62
Broc Jones <jon25044@byui.edu>
2026-05-29 22:41:25 -0600

    made it so it can iterate from multiple scriptures. and creates different variables

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -6,12 +6,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        string book = "Matthew";
-        int chapter = 15;
-        int startVerse = 1;
-        int endVerse = 5;
-        string text = "1 Then came to Jesus scribes and Pharisees, which were of Jerusalem, saying, 2 Why do thy disciples transgress the tradition of the elders? for they wash not their hands when they eat bread. 3 But he answered and said unto them, Why do ye also transgress the commandment of God by your tradition? 4 For God commanded, saying, Honour thy father and mother: and, He that curseth father or mother, let him die the death. 5 But ye say, Whosoever shall say to his father or his mother, It is a gift, by whatsoever thou mightest be profited by me;";
+        List<string> reference = new List<string>();
+        reference.Add("Matthew;15;1;5");
+        reference.Add("John;3;6");
+        reference.Add("Proverbs;3;5;6");
+        reference.Add("2 Nephi;31;20");
+        reference.Add("1 Nephi;3;5");
+
+        Dictionary<string,string> scriptureText = new Dictionary<string, string>();
+        scriptureText.Add("Matthew;15;1;5","1 Then came to Jesus scribes and Pharisees, which were of Jerusalem, saying, 2 Why do thy disciples transgress the tradition of the elders? for they wash not their hands when they eat bread. 3 But he answered and said unto them, Why do ye also transgress the commandment of God by your tradition? 4 For God commanded, saying, Honour thy father and mother: and, He that curseth father or mother, let him die the death. 5 But ye say, Whosoever shall say to his father or his mother, It is a gift, by whatsoever thou mightest be profited by me;");
+        scriptureText.Add("John;3;6", "6 That which is born of the flesh is flesh; and that which is born of the Spirit is spirit.");
+        scriptureText.Add("Proverbs;3;5;6", "5 Trust in the Lord with all thine heart; and lean not unto thine own understanding. 6 In all thy ways acknowledge him, and he shall direct thy paths.");
+        scriptureText.Add("2 Nephi;31;20", "20 Wherefore, ye must press forward with a steadfastness in Christ, having a perfect brightness of hope, and a love of God and of all men. Wherefore, if ye shall press forward, feasting upon the word of Christ, and endure to the end, behold, thus saith the Father: Ye shall have eternal life.");
+        scriptureText.Add("1 Nephi;3;5", "5 And now, behold thy brothers murmur, saying it is a hard thing which I have required of them; but behold I have not required it of them, but it is a commandment of the Lord.");
+
+        Random random = new Random();
+        int x = random.Next(reference.Count);
+        string[] split = reference[x].Split(';');
+        string book = split[0];
+        int chapter = StringToInt(split[1]);
+        int startVerse = StringToInt(split[2]);
+        int endVerse = 0;
+        if (split.Length == 4)
+        {
+            endVerse = StringToInt(split[3]);
+        }
+        string text = scriptureText[reference[x]];
         Scripture myScripture = new Scripture(book, chapter, startVerse, endVerse, text);
+
         int end = 40000;
         myScripture.ShowScripture();
         Console.WriteLine("Welcome to the scripture memorizer program! Press enter to continue ");
@@ -29,6 +51,11 @@ class Program
                 end = 0;
             }
             Console.Clear();
+        }
+
+        static int StringToInt(string num)
+        {
+            return int.Parse(num);
         }
     }
 }
