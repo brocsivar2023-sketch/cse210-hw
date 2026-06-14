65382762b37307d8fecb303970d248319805c3ec
Broc Jones <jon25044@byui.edu>
2026-06-13 22:55:34 -0600

    Added a personal touch to the program it keeps track of how many times they did each activity and what they did.

--- prove/Develop04/Menu.cs
+++ prove/Develop04/Menu.cs
@@ -3,6 +3,9 @@ class Menu
     public void RunMenu()
     {
         int var = 0;
+        int breathing = 0;
+        int reflection = 0;
+        int lisiting = 0;
         while(var != 4)
         {
             Console.WriteLine("Menu Options: ");
@@ -19,18 +22,21 @@ class Menu
                 Breathing myBreathing = new Breathing("This activity will help you with your breathing by walking you through how to breathe in and out slowly. Clear your mind and focus on your breathing. ");
                 myBreathing.RunActivity();
                 Console.Clear();
+                breathing ++;
             }
             else if (var == 2)
             {
                 Reflection myReflection = new Reflection("This activity will help you reflect on times in your life when youi have shown stregnth and resilience. This will help you recognize these things in other aspects of your life. ");
                 myReflection.RunActivity();
                 Console.Clear();
+                reflection ++;
             }
             else if (var == 3)
             {
                 Listing myListing = new Listing("This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.");
                 myListing.RunActivity();
                 Console.Clear();
+                lisiting ++;
             }
             else if (var == 4)
             {
@@ -44,5 +50,9 @@ class Menu
             }
 
         }
+        Console.WriteLine($"You completed the Breathing Activity {breathing} times, the Reflection Activity {reflection} times, and the Listing Activity {lisiting} times ");
+        Console.WriteLine("Please press enter to end program. ");
+        Console.ReadLine();
+        Console.WriteLine("Thanks for Participating in theses Activities!! ");
     }
 }
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,6 +1,13 @@
 using System;
 using Microsoft.VisualBasic;
 
+
+/* 
+For my program I completed all of the rquirements and I made 4 different classes. They each perform different
+funtions. For my exceeding requirements I added a part where it keeps track of how many times you did each activity
+and prints out a message at the end of the program that lets you know what activitys you did and how many times 
+you did it.
+*/
 class Program
 {
     static void Main(string[] args)
