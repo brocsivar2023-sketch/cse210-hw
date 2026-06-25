13823d9b200451b0c22eaddca74df3a10f3c6bb5
Broc Jones <jon25044@byui.edu>
2026-06-25 10:39:09 -0600

    Added the menu class also added stuff to the Progrm.cs class

---
+++ prove/Develop05/Menu.cs
@@ -0,0 +1,27 @@
+class Menu
+{
+    public int DisplayMenu()
+    {
+        //Put in number of points that the person has
+        Console.WriteLine();
+        Console.WriteLine("Menu Options: ");
+        Console.WriteLine("  1. Create a new Goal ");
+        Console.WriteLine("  2. List Goals ");
+        Console.WriteLine("  3. Save Goals ");
+        Console.WriteLine("  4. Load Goals ");
+        Console.WriteLine("  5. Record Event ");
+        Console.WriteLine("  6. Quit ");
+        Console.WriteLine("Select a choice from menu: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    public int DisplayGoalMenu()
+    {
+        Console.WriteLine("The types of goals are: ");
+        Console.WriteLine("  1. Simple Goals ");
+        Console.WriteLine("  2. Eternal Goals ");
+        Console.WriteLine("  3. Checklist Goals ");
+        Console.WriteLine("Which type of goal would you like to create? ");
+        return int.Parse(Console.ReadLine());
+    }
+}
--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -13,10 +13,61 @@ class Program
         myBase.MarkComplete();
         Console.WriteLine(myBase.GetDisplayString());
         */
-        SimpleGoal myGoal = new SimpleGoal();
+       /* SimpleGoal myGoal = new SimpleGoal();
         myGoal.CreateGoal();
         Console.WriteLine(myGoal.GetDisplayString());
         myGoal.RecordEvent();
-        Console.WriteLine(myGoal.GetDisplayString());
+        Console.WriteLine(myGoal.GetDisplayString()); */
+
+        int loopNum = 0;
+        while (loopNum != 6)
+        {
+            Menu menu = new Menu();
+            loopNum = menu.DisplayMenu();
+            if (loopNum == 1)
+            {
+                int goalNum = menu.DisplayGoalMenu();
+                if (goalNum == 1)
+                {
+                    
+                }
+                else if (goalNum == 2)
+                {
+                    
+                }
+                else if (goalNum == 3)
+                {
+                    
+                }
+                else
+                {
+                    Console.WriteLine("You did something wrong. Try again ");
+                }
+            }
+            else if (loopNum == 2)
+            {
+                
+            }
+            else if (loopNum == 3)
+            {
+                
+            }
+            else if (loopNum == 4)
+            {
+                
+            }
+            else if (loopNum == 5)
+            {
+                
+            }
+            else if (loopNum == 6)
+            {
+                Console.WriteLine("You ended the program ");
+            }
+            else
+            {
+                Console.WriteLine("You did something wrong. Try again ");
+            }
+        }
     }
 }
