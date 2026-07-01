15a944f8199383be3181f53df9f72a2776bf7326
Broc Jones <jon25044@byui.edu>
2026-07-01 11:23:18 -0600

    finished up program

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -13,6 +13,20 @@ class Program
 
         videos.Add(video1);
 
+        Video video2 = new Video("Funny Cats", "Katy", 20);
+        video2.AddComments("Julianna", "I love cats");
+        video2.AddComments("Kristy", "I hate cats");
+        video2.AddComments("Susana", "I need milk");
+
+        videos.Add(video2);
+
+        Video video3 = new Video("Funny People", "Gary", 100);
+        video3.AddComments("Ash", "I can't beleive I found this video!");
+        video3.AddComments("Misty", "This video is unbelievable");
+        video3.AddComments("Brock", "I love rocks");
+
+        videos.Add(video3);
+
         foreach (Video video in videos)
         {
             Console.WriteLine(video.Print());
