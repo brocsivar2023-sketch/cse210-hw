348869885e837f60a8795be450cea57c0f91ddf0
Broc Jones <jon25044@byui.edu>
2026-07-17 16:54:34 -0600

    Added cycling class

---
+++ final/Foundation4/Cycling.cs
@@ -0,0 +1,27 @@
+class Cycling : Activity
+{
+    private double _speed;
+
+    public Cycling() : base()
+    {
+        _speed = 1;
+    }
+    public Cycling(string date, int length, int speed): base(date, length, "Cycling")
+    {
+        _speed = speed;
+    }
+
+    protected override double Speed()
+    {
+        return _speed;
+    }
+    protected override double Pace()
+    {
+        return 60 / _speed;
+    }
+
+    protected override double Distance()
+    {
+        return _speed / GetLength() * 60;
+    }
+}
--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -9,5 +9,9 @@ class Program
         Running running = new Running("7 July 2026", 45, 2);
         _activityList.Add(running);
         Console.WriteLine(running.GetSummary());
+
+        Cycling cycling = new Cycling("10 July 2026", 30, 15);
+        _activityList.Add(cycling);
+        Console.WriteLine(cycling.GetSummary());
     }
 }
