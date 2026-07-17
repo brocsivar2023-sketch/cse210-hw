db13435b8383cebeb3b535599a447b47eb39034c
Broc Jones <jon25044@byui.edu>
2026-07-17 15:05:50 -0600

    Added the base events clas

---
+++ final/Foundation3/Events.cs
@@ -0,0 +1,44 @@
+class Events
+{
+    private string _eventTitle;
+    private string _description;
+    private string _date;
+    private string _time;
+    private Address _address;
+
+    public Events()
+    {
+        _eventTitle = "";
+        _description = "";
+        _date = "";
+        _time = "";
+        _address = new Address();
+    }
+    public Events(string eventTitle, string description, string date, string time)
+    {
+        _eventTitle = eventTitle;
+        _description = description;
+        _date = date;
+        _time = time;
+    }
+
+    public void SetAddress(string street, string town, string state, string country)
+    {
+        _address = new Address(street, town, state, country);
+    }
+    private string AddressString()
+    {
+        return _address.AddressString();
+    }
+
+    public string StandardDetails()
+    {
+        return $"Event: {_eventTitle}, Description: {_description}, Date: {_date}, Time: {_time}, Address: {AddressString()}";
+    }
+
+    public virtual string ShortDetails()
+    {
+        return $"Title: {_eventTitle}, Date: {_date}";
+
+    }
+}
--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -4,8 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Address address1 = new Address("14 Road 2894", "Aztec", "New Mexico", "USA");
+        // Address address1 = new Address("14 Road 2894", "Aztec", "New Mexico", "USA");
 
-        Console.WriteLine(address1.AddressString());
+        // Console.WriteLine(address1.AddressString());
+
+        Events event1 = new Events("Popcorn Party", "A party where everyone makes a different type of popcorn", "5/14/2027", "6:00");
+        event1.SetAddress("14 Road 2894", "Aztec", "New Mexico", "USA");
+        Console.WriteLine(event1.StandardDetails());
+        Console.WriteLine(event1.ShortDetails());
     }
 }
